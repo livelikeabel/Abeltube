7d380c13ea41fcf196e340aeb4ce7896cb1fc0d1
Abel <esung1129@gmail.com>
2019-06-06 00:54:54 +0900

    :art: middleware setting

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from "express";
-const app = express();
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
+const app = express();
 const PORT = 4000;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("hello world!");
