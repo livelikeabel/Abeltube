960af955ef4ba75477394571bc1191c704f8f74e
Abel <esung1129@gmail.com>
2019-06-06 21:47:18 +0900

    :art:template 기본 업 및 login/join페이지 작업

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,16 @@
 export const home = (req, res) => res.render("home", { pageTitle: "home" });
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
   res.render("search", { pageTitle: "search", searchingBy });
 };
+
 export const videos = (req, res) => res.send("Videos", { pageTitle: "Videos" });
+
 export const upload = (req, res) => res.send("Upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
   res.send("Video Detail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
