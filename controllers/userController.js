3fb7af38c08b9a80aa201c25c98290213bbb36b8
Abel <esung1129@gmail.com>
2019-06-07 01:05:05 +0900

    :sparkles: Log in and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,11 @@ export const postJoin = (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const login = (req, res) => res.render("login");
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Log in"});
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.send("Logout");
 export const users = (req, res) => res.render("users");
 export const userDetail = (req, res) => {
