f49c6ef43b89bea363e39cc121d7f67cba4d8002
Abel <esung1129@gmail.com>
2019-06-06 14:35:17 +0900

    :sparkles:add MVC pattern

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -13,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => console.log("user index"));
-userRouter.get("/edit", (req, res) => console.log("user edit"));
-userRouter.get("/password", (req, res) => console.log("user password"));
