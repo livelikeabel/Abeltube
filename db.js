7ea9ed2f9b8f66d170ed9f60b7b2bc17e2a3586f
Abel <esung1129@gmail.com>
2019-06-09 21:16:39 +0900

    :sparkles:connecting to mongo db

--- db.js
+++ db.js
@@ -1,50 +1,14 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.ogg",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 1212121,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.ogg",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 55555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.ogg",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 11111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.ogg",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/abel-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🗃  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
