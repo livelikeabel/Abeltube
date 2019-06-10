9657e8163592d5c7954924bb90b63697fab6fbad
Abel <esung1129@gmail.com>
2019-06-10 12:54:03 +0900

    :art:render Video detail by ID

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -35,8 +35,18 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (err) {
+    console.log(err);
+    res.redirect(routes.home);
+  }
+};
 export const editVideo = (req, res) =>
   res.send("Edit Video", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
