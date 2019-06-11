275db05b5d7766f0a6c3000d8677e12ce178bb68
Abel <esung1129@gmail.com>
2019-06-11 16:04:28 +0900

    :art: delete a video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,7 +41,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (err) {
     console.log(err);
     res.redirect(routes.home);
@@ -60,16 +60,23 @@ export const getEditVideo = async (req, res) => {
 };
 export const postEditVideo = async (req, res) => {
   const {
-    params: { _id },
+    params: { id },
     body: { title, description }
   } = req;
   try {
-    await Video.findOneAndUpdate({ _id }, { title, description });
-    res.redirect(routes.videoDetail(_id));
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
   } catch (err) {
     res.redirect(routes.home);
   }
   res.render("editVideo", { pageTitle: "Edit Video" });
 };
-export const deleteVideo = (req, res) =>
-  res.send("Delete Video", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (err) {}
+  res.redirect(routes.home);
+};
--- routes.js
+++ routes.js
@@ -32,7 +32,7 @@ const routes = {
   upload: UPLOAD,
   videoDetail: id => (id ? `/videos/${id}` : VIDEO_DETAIL),
   editVideo: id => (id ? `/videos/${id}/edit` : EDIT_VIDEO),
-  deleteVideo: DELETE_VIDEO
+  deleteVideo: id => (id ? `/videos/${id}/delete` : DELETE_VIDEO)
 };
 
 export default routes;
